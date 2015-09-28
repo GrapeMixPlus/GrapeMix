a0a79e32963fd93f9f9df5dd7a138d47ab22844a
SantiagoMoyano <santi.cba.cab.98@gmail.com>
2015-09-28 08:04:12 -0300

    Language changes

--- GMP/urls.py
+++ GMP/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
-                       url(r'^blog/$', "GMP.views.home", name='blog'),
-                       url(r'^cont/$', "GMP.views.cont", name='cont'),
+                       url(r'^$', 'GMP.views.inup', name='inup'),
+                       url(r'^home/$', "GMP.views.home", name='home'),
+                       url(r'^login/$', "GMP.views.login", name='login'),
+                       url(r'^logup/$', "GMP.views.logup", name='logup'),
                       )
--- GMP/views.py
+++ GMP/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response, render, redirect
 
-# Create your views here.
+def inup(request):
+    return render_to_response('grapemix.html')
+
+def login(request):
+    return render_to_response('login.html')
+
+def logup(request):
+    return render_to_response('logup.html')
+
+def home(request):
+    return render_to_response('home.html')
