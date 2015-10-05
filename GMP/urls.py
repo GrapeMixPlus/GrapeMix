17b9180528cd4fd1146dd5bc49db6bc2d8494a3e
SantiagoMoyano <santi.cba.cab.98@gmail.com>
2015-10-05 08:45:07 -0300

    Log In Succes

--- GMP/urls.py
+++ GMP/urls.py
@@ -3,9 +3,8 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
                        url(r'^$', 'GMP.views.inup', name='inup'),
                        url(r'^home/$', "GMP.views.home", name='home'),
-                       url(r'^login/$', 'django.contrib.auth.views.login',
-                           {'template_name':'login.html'}, name='login'),
-                       url(r'^out/$' , 'django.contrib.auth.views.logout_then_login',
+                       url(r'^login/$', 'GMP.views.log_in', name='login'),
+                       url(r'^out/$' , 'GMP.views.log_out',
                            name='logout'),
                        url(r'^logup/$', "GMP.views.logup", name='logup'),
                       )
