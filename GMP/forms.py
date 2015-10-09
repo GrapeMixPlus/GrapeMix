1775931d42b88aca8810d08edf9b145249416d1f
satissera <piojotissera@gmail.com>
2015-10-09 09:59:17 -0300

    base

---
+++ GMP/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.forms.utils import ErrorList
+from django.contrib.auth.models import User
+from GMP.models import UserProfile
+import logging
+logger = logging.getLogger('django')
+
+class DivErrorList(ErrorList):
+    def __unicode__(self): # __unicode__ on Python 2
+        return self.as_divs()
+    def as_divs(self):
+        if not self:
+            return ''
+        return '<div class="alert alert-danger">%s</div>' \
+    % ''.join(['<div class="alert-danger">%s</div>' % e
+               for e in self])
+
+class MyUserCreationForm(UserCreationForm):
+    error_css_class = 'alert alert-danger'
+    def __init__(self, *args, **kwargs):
+        super(UserCreationForm, self).__init__(*args, **kwargs)
+        for (field_name, field) in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+            if field.required:
+                field.widget.attrs['placeholder'] = 'Requerido'
+                field.label = '* ' + str(field.label)
---
+++ GMP/views.py
@@ -0,0 +1,56 @@
+from django.shortcuts import render_to_response, render, redirect
+from django.template import RequestContext
+from django.core.urlresolvers import reverse
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from GMP.models import UserProfile
+from GMP.forms import MyUserCreationForm
+from django.contrib import messages
+import logging
+logger = logging.getLogger('django')
+import os
+
+def inup(request):
+    return render_to_response('grapemix.html')
+
+def log_out(request):
+    logout(request)
+    context = RequestContext(request)
+    messages.add_message(request, messages.SUCCESS, 'Muchas gracias!')
+    return redirect('/')
+
+def log_in(request):
+    context = RequestContext(request)
+
+    #createUserForm = MyUserCreationForm()
+    args = {}
+    #args['createUserForm'] = createUserForm
+
+    if request.method == 'POST':
+
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user:
+            if user.is_active:
+                login(request, user)
+                return redirect('/home')
+            else:
+                messages.add_message(request, messages.ERROR,
+                                     'El usuario no esta activo')
+                return render_to_response('login.html', args, context)
+        else:
+            messages.add_message(request, messages.ERROR,
+                                 'Invalid User')
+            return render_to_response('login.html', args, context)
+    else:
+        return render_to_response('login.html',args, context)
+
+def logup(request):
+     return render_to_response('logup.html')
+
+@login_required(login_url='/login')
+def home(request):
+    context = RequestContext(request)
+    return render_to_response('home.html', context)
