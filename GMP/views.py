bbd3d3575b13e053379ad8359648c7365139e3b5
RodrigoIbarraa <rodrigooibarraa@gmial.com>
2015-10-30 11:55:15 -0300

    playlist

--- GMP/views.py
+++ GMP/views.py
@@ -66,3 +66,7 @@ def logup(request):
 def home(request):
     context = RequestContext(request)
     return render_to_response('home.html', context)
+
+def edit_profile(request):
+    context = RequestContext(request)
+    return render_to_response('edit_profile.html', context)
