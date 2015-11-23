e4d5e67dbdffb3506e1018f3d756fc6e96555cc8
garciamarianoCBA <garciamariano47@yahoo.com>
2015-11-23 14:02:38 -0300

    ----

---
+++ GMP/static/js/init.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+	$('.parallax').parallax();
+    $('.button-collapse').sideNav();
+    $('select').material_select();
+    $('.dropdown-button').dropdown();
+    $('.button-collapse').sideNav();
+    $('ul.tabs').tabs();
+    $('.slider').slider({full_width: true});
+    $('.datepicker').pickadate({selectMonths: true, selectYears: 15});
+    $('.materialboxed').materialbox();
+    $('.modal-trigger').leanModal();
+
+}); // end of document ready
+
+
